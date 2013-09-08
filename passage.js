d6b0d9061e138054afdff32794cc9ca22bf32ee0
Drew Blaisdell <drew.blaisdell@gmail.com>
2013-09-07 18:50:03 -0700

    Initial commit.

---
+++ passage.js
@@ -0,0 +1,188 @@
+(function(){
+	window.passage = {
+		color: 'rgba(100,100,100,0.5)',
+		reverseColor: 'rgba(200,200,100,0.5)',
+		complete: false,
+		decaySpeed: 0.02,
+		index: 0,
+		progress: 0,
+		radius: 30,
+		stroke: 10,
+		sensitivity: .02,
+		sleepPeriod: 1000,
+		threshold: 100,
+	};
+
+	function Spinner(){
+		this.createSpinner = function(){
+			var canvas = document.createElement('canvas');
+			canvas.id = 'passage';
+			canvas.width = passage.width;
+			canvas.height = passage.height;
+			canvas.style.position = 'fixed';
+			canvas.style.bottom = '5px';
+			canvas.style.right = '5px';
+
+			passage.context = canvas.getContext('2d');
+			document.body.appendChild(canvas);
+		};
+
+		this.updateSpinner = function(percent){
+			if(passage.sleepTimer){
+				clearTimeout(passage.sleepTimer);
+			}
+
+			if(percent < 1 && percent > -1){
+				passage.sleepTimer = setTimeout(function(){
+					passage.spinner.decaySpinner();
+				}, passage.sleepPeriod);
+			}
+			
+			drawArc(percent);
+		};
+
+		this.decaySpinner = function(){
+			passage.complete = false;
+			if(passage.progress === 0){
+				return clearTimeout(passage.decayTimer);
+			}
+
+			if(passage.progress > 0){
+				passage.progress = (passage.progress > passage.decaySpeed) ? passage.progress - passage.decaySpeed : 0;
+			} else {
+				passage.progress = (passage.progress < passage.decaySpeed) ? passage.progress + passage.decaySpeed : 0;
+			}
+			
+			drawArc(passage.progress);
+
+			passage.decayTimer = setTimeout(passage.spinner.decaySpinner, 20);
+		};
+
+		this.completeSpinner = function(){
+			var ctx = passage.context;
+
+			ctx.beginPath();
+			ctx.arc(passage.width / 2, passage.height / 2, passage.radius - passage.stroke / 2 - 2, 0, Math.PI * 2, false);
+			ctx.fillStyle = 'rgba(0, 200, 0, 0.5)';
+			ctx.fill();
+
+			setTimeout(function(){
+				passage.spinner.completeSpinnerTick(30);
+			}, 500);
+			
+		};
+
+		this.completeSpinnerTick = function(extra){
+			if(extra < 0){
+				return this.completeSpinnerEnd();
+			}
+			
+			drawArc(extra / 30);
+
+			var ctx = passage.context;
+			ctx.beginPath();
+			ctx.arc(passage.width / 2, passage.height / 2, (passage.radius - passage.stroke / 2 - 2) * (extra / 30), 0, Math.PI * 2, false);
+			ctx.fillStyle = 'rgba(0, 200, 0, 0.5)';
+			ctx.fill();
+
+			setTimeout(function(){
+				passage.spinner.completeSpinnerTick(extra - 1);
+			}, 20);
+		};
+
+		this.completeSpinnerEnd = function(){
+			passage.complete = false;
+			passage.progress = 0;
+		};
+	}
+
+	var init = function(){
+		passage.width = passage.radius * 2 + passage.stroke * 2;
+		passage.height = passage.radius * 2 + passage.stroke * 2;
+		passage.sections = document.getElementsByTagName('section');
+		passage.spinner = new Spinner();
+		passage.spinner.createSpinner();
+
+		document.addEventListener('mousewheel', mouseWheelHandler);
+	};
+
+	var mouseWheelHandler = function(event){
+		var event = window.event || event,
+			delta = (event.wheelDeltaY < 0) ? event.wheelDeltaY : event.wheelDeltaY,
+			increment = -delta / 100;
+
+		if(passage.decayTimer){
+			clearTimeout(passage.decayTimer);
+		}
+
+		passage.progress = Math.min(1, passage.progress + increment * passage.sensitivity);
+
+		if(passage.progress === 1 && !passage.complete){
+			passage.complete = true;
+			passage.spinner.updateSpinner(passage.progress);
+			passage.spinner.completeSpinner();
+
+			console.log(passage.index);
+			if(passage.index + 1 < passage.sections.length){
+				passage.index++;
+				goToSection(passage.index);
+			}
+		} else if (!passage.complete){
+			passage.spinner.updateSpinner(passage.progress);
+		}
+
+		event.preventDefault();
+	};
+
+	var goToSection = function(index){
+		scrollTo(passage.sections[index], 1000);
+	};
+
+	var scrollTo = function(element, duration) {
+        if (duration < 0) return;
+        var difference = window.scrollY - element.offsetTop;
+        var perTick = -difference / duration * 10;
+		if(isNaN(perTick)){
+			return false;
+		}
+
+        setTimeout(function() {
+            document.body.scrollTop += perTick;
+            scrollTo(element, duration - 10);
+        }, 10);
+    };
+
+	var drawArc = function(percent){
+		var ctx = passage.context,
+			reverse = (percent < 0) ? true : false;
+		if(percent === 0){
+			ctx.clearRect(0, 0, passage.width, passage.height);
+			return false;
+		} else if(percent === 1 || percent === -1){
+			startAngle = 0;
+			endAngle = Math.PI * 2;
+		} else {
+			if(reverse){
+				startAngle = Math.PI * 1.5;
+				endAngle = Math.PI * 1.5 + Math.PI * 2 * percent;
+			} else {
+				startAngle = Math.PI * 1.5;
+				endAngle = Math.PI * 2 * percent - Math.PI * .5;
+			}
+		}
+
+		ctx.clearRect(0, 0, passage.width, passage.height);
+		ctx.beginPath();
+		ctx.arc(passage.width / 2, passage.height / 2, passage.radius, startAngle, endAngle, reverse);
+		ctx.lineWidth = 10;
+		if(reverse){
+			ctx.strokeStyle = passage.reverseColor;
+		} else {
+			ctx.strokeStyle = passage.color;
+		}
+		ctx.stroke();
+		ctx.closePath();
+	};
+
+	document.addEventListener('DOMContentLoaded', init);
+}());
